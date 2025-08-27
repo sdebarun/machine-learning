9b1caab89206d1ede98345fe1fef57e638e27b7d
Debarun Saha <debarun@Debaruns-MacBook-Pro.local>
2025-08-27 11:33:44 +0900

    restructuring

--- semiconductor-ML/main.py
+++ semiconductor-ML/main.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import seaborn as sns
 
 # Load your CSV file
 data = pd.read_csv('cvd_2d_crystal_data.csv')
